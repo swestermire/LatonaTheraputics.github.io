4d4787c5b8cb6abd6bea50996b5276de00d60a4e
Steven <swestermire@gmail.com>
2016-12-04 18:03:55 -0600

    organized files and project clean up

---
+++ js/indexAngJS.js
@@ -0,0 +1,49 @@
+// this will be the ng app for the index page
+
+console.log("indexAngJS.js file initiated...");
+
+(function(){
+	
+	var app = angular.module("indexApp", ["ngRoute"]);
+
+	app.controller('appendHeaderAndFooter' , appendHeaderAndFooterCtrl);
+
+	app.config(function($routeProvider){
+		console.log("Route Function Hit... ");
+		$routeProvider
+		
+		.when("/", {
+			templateUrl : "index.html"
+		})
+
+		.when("/LatonaHome", {
+			templateUrl : "latonaTheraputicsHome.html"
+		})
+
+	});
+
+	appendHeaderAndFooterCtrl.$inject = ["$scope"];
+	function appendHeaderAndFooterCtrl($scope){
+
+		$scope.title = "This is the scope";
+
+		// this isn't good that I'm mixing jquery with angularJS
+		(function(){
+			console.log("function is working...")
+			
+			// adds the header
+			$.get("layouts/header.html", function(data){
+				console.log('invoked function in appendHeaderAndFooter');
+				$(".header").append(data);
+			})
+
+			// adds the footer
+			$.get("layouts/footer.html" , function(data){
+				$(".footer").append(data);
+			})
+
+		})();
+
+		};
+
+})();
