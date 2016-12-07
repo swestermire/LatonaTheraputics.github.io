12b03a5a2c1d1065f38f83e0ff0fbd34389d7d62
Steven T Westermire <swestermire@gmail.com>
2016-12-07 17:42:11 -0600

    updated about us page with jquery functionality

--- js/indexAngJS.js
+++ js/indexAngJS.js
@@ -6,8 +6,9 @@ console.log("indexAngJS.js file initiated...");
 	
 	var app = angular.module("indexApp", ["ngRoute"]);
 
-	app.controller('indexGeneralFunctions' , indexGeneralFunctionsCtrl)
+	app.controller('indexGeneralFunctions' , indexGeneralFunctionsCtrl);
 	app.controller('appendHeaderAndFooter' , appendHeaderAndFooterCtrl);
+	app.controller('aboutUsPortraits' , aboutUsPortraitCtrl);
 
 	app.config(function($routeProvider){
 		console.log("Route Function Hit... ");
@@ -62,4 +63,151 @@ console.log("indexAngJS.js file initiated...");
 
 		};
 
+	aboutUsPortraitCtrl.$inject = ['$scope'];
+	function aboutUsPortraitCtrl($scope){
+		$bioPortraits = $('.bio-portrait');
+		$bioCollection = $('.portrait-collection');
+		$latonaTeamTitle = $('#latona-team-title');
+		$biography = $('.biography');
+
+		// click event function that brings up portrait when portrait is clicked
+		$scope.portraitFocus = function(event){
+			console.log("Click logged");
+			console.log("clicked portrait is = " + event.target.id);
+
+			var targetedID = event.target.id;
+
+			resetPortrait();
+			expandPortrait(targetedID);
+			deExpandedPortrait();
+			screenLoad();
+		};
+
+		/// expands portrait
+		function expandPortrait(targetedID){
+			$('#' + targetedID).removeClass('bio-portrait').addClass('expanded-portrait');
+		};
+
+		/// de-expands portraits
+		function deExpandedPortrait(){
+			$('.bio-portrait').each(function(){
+				$(this).css({"height" : 100 , "width" : 100});
+			})
+		};
+
+		/// resets the status of the portraits
+		function resetPortrait(){
+			$('.expanded-portrait').removeClass('expanded-portrait').addClass('bio-portrait')
+		};
+
+
+		/// this function creates a separate portrait.  Right now it will not be implemented
+		function createFocusedPortrait(targettedID){
+			console.log("createFocusedPortrait initiated...")
+			$portraitUnfocused = $(".portrait-unfocused");
+			$portraitFocused = $(".portrait-focused");
+
+			// will need to input the targettedID to update the biography
+			if ($portraitUnfocused.length >= 1){
+				$($portraitUnfocused).removeClass("portrait-unfocused").addClass("portrait-focused");
+				var $portraitFocused = $('.portrait-focused');
+
+				var parentWidth = pareseFloat($($portraitFocused).parent().width());
+				var portraitFocusedWidth = parseFloat($($portraitFocused).outerWidth(true));
+
+				var portraitPositionLeft = (parentWidth - portraitFocusedWidth)/2;
+
+				// might want to create another function to position the focused portrait
+				$($portraitFocused).css({"left" : portraitPositionLeft, 
+										 "top" : 600 , 
+										 "position" : "absolute"});
+
+			} else {
+
+			}
+
+		};
+
+		/// function to reposition bio-portraits so they are in the center of the screen
+		function centerAlignPortraits(){
+
+			var parentWidth = $($bioPortraits).parent().width();
+			var portraitMarginRight = parseFloat($($bioPortraits).css('margin-right'));
+			var numPortraits = $($bioPortraits).length;
+			var portraitWidth = $($bioPortraits).width();
+			var portraitBorder = parseFloat($($bioPortraits).css("border-right"));
+			
+			var portraitCollectionWidth = portraitMarginRight *(numPortraits-1) + numPortraits * portraitWidth + numPortraits*2*portraitBorder;
+			var marginLeftPortraitCollection = (parentWidth - portraitCollectionWidth)/2;
+
+			$('.bio-portrait:first-child').css({'margin-left' : marginLeftPortraitCollection});
+		}
+		
+		function adjustBiographyPlacement(){
+			var bioPortraitHeight = $($bioPortraits).outerHeight(true);
+			var bioTitleHeight = $($latonaTeamTitle).outerHeight(true);
+			var bioCollectionHeight = $($bioCollection).outerHeight(true);
+			
+			var biographyHeight = function(){
+				var $portraitFocused = $(".portrait-focused");
+
+				console.log("$portraitFocused  = " + $portraitFocused )
+				if ($portraitFocused != undefined){
+					return $($portraitFocused).height();
+				} else {
+					return 0;
+				}
+			}();
+
+			var biographyTop = bioPortraitHeight + bioTitleHeight + bioCollectionHeight;
+
+			console.log("biographyHeight = " + biographyHeight);
+
+			var parentWidth = $($biography).parent().width();
+			var biographyWidth = $($biography).outerWidth(true);
+			var biographyLeft = (parentWidth - biographyWidth)/2;
+
+			$($biography).css({'left' : biographyLeft,"top": biographyTop ,"position" : "absolute"});
+		}
+
+
+		/// Master list of functions that are carried out when screen elements are loaded/adjusted
+		function screenLoad(){
+			centerAlignPortraits();
+			adjustBiographyPlacement();
+		}
+
+		/// adjusts elements when window is adjusted
+		// $( window ).resize(screenLoad());
+		$( window ).resize(function(){
+			screenLoad();
+		});
+
+		/// !!! Not sure if this really works...
+		/// setups screen elements when window is first loaded
+		$( window ).ready(function(){
+			screenLoad();
+		});
+	};
+
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
