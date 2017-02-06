2300ac1f7711a2da9489004bb70ff568b043ea10
Steven <swestermire@gmail.com>
2017-02-06 13:20:08 -0600

    added additional timeline functionality

--- js/latonaTheraputicsAngJS.js
+++ js/latonaTheraputicsAngJS.js
@@ -3,9 +3,11 @@
 console.log("latonaTheraputicsAngJS initiated...");
 
 (function(){
-	var app = angular.module("latonaTheraputicsAngJS", [])
+	var app = angular.module("latonaTheraputicsAngJS", []);
 
 	app.controller("articleGenerator" , articleGeneratorCtrl);
+	// .directive("timelineCenterBlock", timelineCenterBlockDirective);
+
 	app.controller("appendHeaderAndFooter" , appendHeaderAndFooterCtrl);
 	// app.controller("appendTimelineInfo" , appendTimelineInfoCtrl);
 
@@ -13,7 +15,6 @@ console.log("latonaTheraputicsAngJS initiated...");
 	// function appendTimelineInfoCtrl($scope){
 	// 	/// $scoped.entry was here before. but this controller doesn't work
 	// };
-
 
 	// Strange... when I inject articleBlock templates to the DOM, the scoped entry data
 	// cannot display.  I've tried using alternative controllers, declaring the controller
@@ -31,6 +32,7 @@ console.log("latonaTheraputicsAngJS initiated...");
 
 		  	/// calls function to populate article info into article blocks
 		  	populateArticleBlocksInfo();
+		  	repositionTimelineArticles();
 		  });
 
 		console.log("pre-DOM load check")
@@ -46,7 +48,6 @@ console.log("latonaTheraputicsAngJS initiated...");
 		  		var timelineArticles = response.data;
 
 		  		/// populating article blocks with content
-		  		console.log("allArticleBlocks length = " + allArticleBlocks.length)
 		  		for (var idx = 0; idx < allArticleBlocks.length; idx++){
 		  			var timelineArticle = timelineArticles[String(idx)];
 		  			populateArticleBlockInfo(allArticleBlocks[idx], timelineArticle, idx, "article-block-container");
@@ -75,9 +76,62 @@ console.log("latonaTheraputicsAngJS initiated...");
 			};
 		};
 
+		function repositionTimelineArticles(){
+			/// identify all article elements
+			var allArticleBlocks = angular.element(document.getElementsByClassName('article-block-container'));
+			var timelineWindow = angular.element(document.getElementsByClassName('timeline-window'))[0];
+
+			/// Everything is relative to timeline-window
+			
+			/// variable to adjust timeline-window height.  Needed for content placement below timeline (e.g. footer)
+			var totHeight = 0;
+
+			/// adjust timeline-window placement to be centered relative to screen
+			elementCenter(window, timelineWindow);
+
+			/// adjust articles to be centered within timeline-window
+
+			console.log('timelineWindow.style.width = ' + timelineWindow.offsetWidth);
+
+			// for (var idx = 0; idx < allArticleBlocks.length; idx++){
+			// 	var article = allArticleBlocks[idx];
+			// 	var prevArticle = allArticleBlocks[idx-1];
+			// 	if (idx % 2 == 0){
+			// 		article.style.left = 10 + 'px';
+			// 		article.style.top = 10 + 'px';
+
+			// 		console.log("article.style.bot = " + article.style.bottom);
+			// 		totHeight += article.offsetHeight;
+
+			// 	} else {
+			// 		article.style.left = '50vw';
+			// 		article.style.top = 10 + 'px';
+			// 	}
+
+			// 	/// adjusting timelineWindow height.
+			// 	timelineWindow.style.height = totHeight;
+			// 	console.log("timelineWindow.style.height = " + timelineWindow.style.height)
+				
+			// };
+
+			/// calculate overall article height
+
+			/// identify all timeline balls
+				/// we can probably just add the timeline balls at the end when articles are positioned correctly
+		};
+
+		/// centers element relative to a referened element
+		function elementCenter(reference, element){
+			var refWidth = reference.innerWidth;
+			var elementWidth = element.offsetWidth;
+
+			var margin = Math.floor((refWidth - elementWidth)/2);
+			element.style.marginLeft = margin + 'px';
+		}
+
 		$scope.titleAlt = "Alternate Title Block";
 
-		// !! AngJS implementation of JSON get request
+		// !! AngJS implementation of JSON getjjjhuuuu request
 		// a filter could be added to look for a date range, article types, etc... Will add later
 		$http.get('../../public/timelineEvents.json').success(function(response, searchParams){
 			
@@ -98,7 +152,31 @@ console.log("latonaTheraputicsAngJS initiated...");
 			$compile($(".articleRight").html(template).contents());
 		});
 
+		$templateRequest("../../layouts/timelineCenterBlock.html").then(function(template){
+			$compile($(".timelineCenterBlock").html(template).contents());
+		});
+
 		};
+
+
+/// Custom Directive
+
+	/// timeline bar generating directive
+	/// this doesn't work...
+	// function timelineCenterBlockDirective(){
+
+	// 	/// factory function, ddo = directive definition object
+	// 	var ddo = {
+	// 		/// link : ???? Adding additional behavior before and after directive i presume.
+	// 		// restrict : "E",
+	// 		templateURL: '../../layouts/timelineCenterBlock.html'
+	// 	};
+
+	// 	console.log("templateURL = " + ddo["templateURL"])
+	// 	return ddo;
+	// };
+
+
 
 	// controller to fetch header and footer template
 	appendHeaderAndFooterCtrl.$inject = ["$templateRequest", "$compile"];
